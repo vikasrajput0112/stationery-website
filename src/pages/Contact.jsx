09221a8b7c524a417580d680e0ac82d9ae1d0d0c
vikasrajput0112 <devops1@paisalo.in>
2026-03-30 12:21:17 +0530

    Create Contact.jsx

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Contact Us</h1>
+      <p>Email: support@stationery.com</p>
+      <p>Phone: +91-9999999999</p>
+    </div>
+  );
+};
+
+export default Contact;
