6709d2b1c1cd57fc5c467066512b0f2cf67b589a
vikasrajput0112 <devops1@paisalo.in>
2026-03-31 14:10:14 +0530

    Create Order.jsx

---
+++ src/pages/Order.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Orders = () => {
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>📦 My Orders</h1>
+
+      <p>You have no orders yet.</p>
+    </div>
+  );
+};
+
+export default Orders;
